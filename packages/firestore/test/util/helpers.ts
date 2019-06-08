e2de63452aba08963deab23bf002fbbe71175c3d
Feiyang1 <plane1113@gmail.com>
2019-06-07 17:46:49 -0700

    migrate Firestore to eslint

--- packages/firestore/test/util/helpers.ts
+++ packages/firestore/test/util/helpers.ts
@@ -96,7 +96,7 @@ export type TestSnapshotVersion = number;
  */
 export const DELETE_SENTINEL = '<DELETE>';
 
-const preConverter = (input: unknown) => {
+const preConverter = (input: unknown): unknown => {
   return input === DELETE_SENTINEL ? FieldValueImpl.delete() : input;
 };
 
@@ -412,8 +412,8 @@ export function localViewChanges(
   targetId: TargetId,
   changes: { added?: string[]; removed?: string[] }
 ): LocalViewChanges {
-  if (!changes.added) changes.added = [];
-  if (!changes.removed) changes.removed = [];
+  if (!changes.added) {changes.added = [];}
+  if (!changes.removed) {changes.removed = [];}
 
   let addedKeys = documentKeySet();
   let removedKeys = documentKeySet();
